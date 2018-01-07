6a9f547636673a1f82cbe38c3cb8dd418f3b8929
steffi <spolisetty.work@gmail.com>
2018-01-07 10:52:52 -0500

    removed secretkey

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8!##k+&@!p82rkmn2ufvxjz%6y#6zs1fphbl!#ilknva&!(@f&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
